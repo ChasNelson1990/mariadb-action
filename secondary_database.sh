5d0c27e537ba5bb5068ee7683c7537bfcd3fde6c
Chas Nelson <cvs@chasnelson.co.uk>
2020-12-17 16:40:37 +0000

    use mysql_note instead of echo in secondary entrypoint

--- secondary_database.sh
+++ secondary_database.sh
@@ -2,15 +2,15 @@
 
 # Creates a custom database and user if specified
 if [ -n "$INPUT_SECONDARY_DATABASE" ]; then
-    echo "Use specified secondary database"
+    mysql_note "Use specified secondary database"
 
     if [ -n "$INPUT_ROOT_PASSWORD" ]; then
-        echo "...with root user and root password"
+        mysql_note "...with root user and root password"
 
         docker_process_sql --database=mysql <<<"CREATE DATABASE IF NOT EXISTS \`$INPUT_SECONDARY_DATABASE\` ;"
         docker_process_sql --database=mysql <<<"GRANT ALL ON \`${INPUT_SECONDARY_DATABASE}\`.* TO 'root'@'%' ;"
     elif [ -n "$INPUT_MYSQL_USER" ]; then
-        echo "...with specified user and password"
+        mysql_note "...with specified user and password"
 
         docker_process_sql --database=mysql <<<"CREATE DATABASE IF NOT EXISTS \`$INPUT_SECONDARY_DATABASE\` ;"
         docker_process_sql --database=mysql <<<"GRANT ALL ON \`${INPUT_SECONDARY_DATABASE}\`.* TO 'INPUT_MYSQL_USER'@'%' ;"
