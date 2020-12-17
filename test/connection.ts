1a40e32d07643dc27ec49b86e63125b9a8ae381f
Chas Nelson <cvs@chasnelson.co.uk>
2020-12-17 11:57:26 +0000

    mvoe tests to two databases

--- test/connection.ts
+++ test/connection.ts
@@ -4,40 +4,40 @@ import { createConnection } from 'mysql';
 import { required } from './arguments';
 
 export interface Queryer {
-  init(): Promise<void>;
-  query(sql: string): Promise<any>;
-  close(): Promise<void>;
+    init(): Promise<void>;
+    query(sql: string): Promise<any>;
+    close(): Promise<void>;
 }
 
-function createQueryer(): Queryer {
-  const connection = createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: required('password'),
-    database: 'test',
-  });
+function createQueryer(db: string): Queryer {
+    const connection = createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: required('password'),
+        database: db,
+    });
 
-  return {
-    init: () => new Promise<void>((resolve, reject) => {
-      connection.connect((err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }),
-    query: promisify(connection.query.bind(connection)),
-    close: () => new Promise<void>((resolve, reject) => {
-      connection.end((err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }),
-  };
+    return {
+        init: () => new Promise<void>((resolve, reject) => {
+            connection.connect((err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        }),
+        query: promisify(connection.query.bind(connection)),
+        close: () => new Promise<void>((resolve, reject) => {
+            connection.end((err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        }),
+    };
 }
 
 export default createQueryer;
--- test/database-test.ts
+++ test/database-test.ts
@@ -3,19 +3,37 @@ import { expect } from 'chai';
 import createQueryer from './connection';
 
 describe('Database', () => {
-  const queryer = createQueryer();
+    const queryer = createQueryer('database');
 
-  before('Connect', queryer.init);
-  after('Disconnect', queryer.close);
+    before('Connect', queryer.init);
+    after('Disconnect', queryer.close);
 
-  describe('#select', () => {
-    it('should return tables for current database', async () => {
-      const results = await queryer.query('SHOW TABLES');
-      expect(results).to.be.a('array');
+    describe('#select', () => {
+        it('should return tables for current database', async () => {
+            const results = await queryer.query('SHOW TABLES');
+            expect(results).to.be.a('array');
+        });
+        it('should return informations for other database', async () => {
+            const results = await queryer.query('SELECT * FROM information_schema.innodb_trx');
+            expect(results).to.be.a('array');
+        });
     });
-    it('should return informations for other database', async () => {
-      const results = await queryer.query('SELECT * FROM information_schema.innodb_trx');
-      expect(results).to.be.a('array');
+});
+
+describe('Test database', () => {
+    const queryer = createQueryer('test_database');
+
+    before('Connect', queryer.init);
+    after('Disconnect', queryer.close);
+
+    describe('#select', () => {
+        it('should return tables for current database', async () => {
+            const results = await queryer.query('SHOW TABLES');
+            expect(results).to.be.a('array');
+        });
+        it('should return informations for other database', async () => {
+            const results = await queryer.query('SELECT * FROM information_schema.innodb_trx');
+            expect(results).to.be.a('array');
+        });
     });
-  });
 });
