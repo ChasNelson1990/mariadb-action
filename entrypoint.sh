75f791e8fab2376a98b5561572a4430c0fe7cd25
Chas Nelson <cvs@chasnelson.co.uk>
2020-12-17 14:18:19 +0000

    add some echoes for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,9 @@ docker_run="$docker_run --character-set-server=$INPUT_CHARACTER_SET_SERVER --col
 sh -c "$docker_run"
 
 echo "Wait for mariadb to be up and running"
-while ! nc -z localhost 3306; do   
+echo nc -z localhost 3306
+while ! nc -z localhost 3306; do
+    echo nc -z localhost 3306
   sleep 0.25 # wait for 1/4 of the second before check again
 done
 
